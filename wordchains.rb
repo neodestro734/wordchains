8a38089bb0aa92512b3b55904307b41039cc0513
Philip Ingram <neodestro734@yahoo.com>
2014-04-06 11:45:26 -0700

    Added dictionary, adjacent words working

--- wordchains.rb
+++ wordchains.rb
@@ -4,12 +4,34 @@ class WordChainer
 	
 	attr_accessor :dictionary #dictionary is a set
 
-	def initialize
-
+	#DONE
+	def initialize(dictionary_file)
+		dict = File.readlines(dictionary_file).map(&:chomp)
+		@dictionary = Set.new(dict)
 	end
 
+	#DONE
 	def adjacent_words(word)
+		@dictionary.select do |d_word|
+			keep_word = false
+			
+			if (d_word.length == word.length)
+				diff_chars = 0
+				
+				d_word.each_char.with_index do |w_char, i|
+					if d_word[i] != word[i]
+						diff_chars += 1
+						break if diff_chars > 1
+					end
+				end
+				keep_word = true if diff_chars == 1
 
+			end
+			
+		end
 	end
-	
+
 end
+
+# wc = WordChainer.new('dictionary.txt')
+# p wc.adjacent_words('cat')
